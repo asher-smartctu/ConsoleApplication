f2c2a07b947b558b93becb1e7f65281463155abc
asher-smartctu <131937241+asher-smartctu@users.noreply.github.com>
2023-05-05 10:14:14 +0200

    Modified program cs to accept a user's name
    Modified program cs to accept a user's name and display the user's name immediately

--- MA_SMART_StudID_FA6_ADP631/Program.cs
+++ MA_SMART_StudID_FA6_ADP631/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+String name;
+
+Console.WriteLine("Please enter your name");
+
+name = Console.ReadLine();
+
+Console.WriteLine("Hello, [0}", name);
+
+Console.ReadKey();
